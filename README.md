f358d0181cf829f11eac80a1ba854e3b8565042a
Angel Johank <angeljohank@gmail.com>
2022-10-19 14:28:45 -0500

    added suggest command

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ IB help discord bot made with Deno ( incomplete )
 create an .env file in the root directory that contains a DISCORD_TOKEN
 ```
 DISCORD_TOKEN="your token here"
+DEVELOPER_ID="your discord id" //optional
 ```
 Finally run this command
 ```
--- main.ts
+++ main.ts
@@ -4,11 +4,13 @@ import {
   EmbedPayload,
   GatewayIntents,
   Message,
+  User,
 } from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 
 // Read .env config
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
+const DEVELOPER_ID = Deno.env.get("DEVELOPER_ID") || undefined;
 const INTENTS = [
   GatewayIntents.DIRECT_MESSAGES,
   GatewayIntents.GUILDS,
@@ -24,15 +26,35 @@ client.setPresence({
   type: "LISTENING",
 });
 
-// Setup commands
+// Help command
 client.on("messageCreate", (msg: Message) => {
-  if (msg.content.toLowerCase() == "ibot help") {
-    const file = Deno.readTextFileSync("embeds/help.json");
-    const config: EmbedPayload = JSON.parse(file);
+  if (msg.content.toLowerCase() !== "ibot help") return;
 
-    const reply = new Embed(config).setTimestamp(Date.now());
-    msg.reply(reply);
+  const file = Deno.readTextFileSync("embeds/help.json");
+  const config: EmbedPayload = JSON.parse(file);
+
+  const reply = new Embed(config).setTimestamp(Date.now());
+  msg.reply(reply);
+});
+
+// Suggest command
+client.on("messageCreate", (msg: Message) => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith("ibot suggest")) return;
+
+  if (DEVELOPER_ID === undefined) {
+    msg.reply("There's no a developer for this bot instance");
+    return;
   }
+
+  // Report suggestion to the developer
+  client.users.fetch(DEVELOPER_ID).then((developer: User) => {
+    developer.send(
+      `**New suggestion from <@${msg.author.id}>:** ${msg.content.slice(12)}`,
+    );
+  });
+
+  msg.reply("Suggestion submited");
 });
 
 // Connect client
