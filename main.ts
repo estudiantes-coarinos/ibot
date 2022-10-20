42a7aae63b013838a287a6ae8d42367a3a84f373
Angel Johank <angeljohank@gmail.com>
2022-10-19 21:35:12 -0500

    make bot setup more readable

--- main.ts
+++ main.ts
@@ -1,57 +1,57 @@
 import {
-  Command,
+  command,
   CommandClient,
   CommandContext,
   Embed,
   EmbedPayload,
+  event,
   GatewayIntents,
 } from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 
 // Read .env config
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
-const client = new CommandClient({
-  prefix: Deno.env.get("PREFIX") || "ibot",
-  intents: [
-    GatewayIntents.DIRECT_MESSAGES,
-    GatewayIntents.GUILDS,
-    GatewayIntents.GUILD_MESSAGES,
-  ],
-});
+class Client extends CommandClient {
+  constructor() {
+    super({
+      prefix: Deno.env.get("PREFIX") || "ibot",
+      intents: [
+        GatewayIntents.DIRECT_MESSAGES,
+        GatewayIntents.GUILDS,
+        GatewayIntents.GUILD_MESSAGES,
+      ],
+    });
+  }
 
-client.setPresence({
-  name: `${client.prefix}help`,
-  type: "LISTENING",
-});
+  @event()
+  ready() {
+    console.log(`Logged in as ${this.user?.tag}!`);
 
-client.on("ready", () => {
-  console.log(`${client.user?.tag} running with prefix "${client.prefix}"`);
-});
+    this.setPresence({
+      name: `${this.prefix}help`,
+      type: "LISTENING",
+    });
+  }
 
-// Setup commands
-const commands = Deno.readDirSync("./commands");
-
-for (const command of commands) {
-  if (!command.isFile && !command.name.endsWith(".ts")) break;
-
-  const commandDir = `./commands/${command.name}`;
-  const module = await import(commandDir);
-  client.commands.add(module.default);
-}
-
-// Help command
-class HelpCommand extends Command {
-  name = "help";
-
-  execute(ctx: CommandContext) {
+  @command({ aliases: "help" })
+  Help(ctx: CommandContext) {
     const file = Deno.readTextFileSync("embeds/help.json");
     const config: EmbedPayload = JSON.parse(file);
-
     const reply = new Embed(config).setTimestamp(Date.now());
     ctx.message.reply(reply);
   }
 }
 
-client.commands.add(HelpCommand);
+// Setup commands
+const client = new Client()
+const commands = Deno.readDirSync("./commands");
+
+for (const command of commands) {
+  if (command.isFile && command.name.endsWith(".ts")) {
+    const commandDir = `./commands/${command.name}`;
+    const module = await import(commandDir);
+    client.commands.add(module.default);
+  }
+}
 
 client.connect();
