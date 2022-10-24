0bb4c24ce6367861c908adbd058d38c9e5788777
Angel Johank <angeljohank@gmail.com>
2022-10-23 21:22:24 -0500

    removing useless comments

--- utils/embed.ts
+++ utils/embed.ts
@@ -5,11 +5,9 @@ import { Embed } from "https://deno.land/x/harmony@v2.6.0/mod.ts";
  * the currect timestamp
  */
 export function getEmbed(embedName: string): Embed {
-  // read file contents
   const embedPath = getEmbedPath(embedName);
   const embedText = Deno.readTextFileSync(embedPath);
 
-  // create an embed
   const date = Date.now();
   return new Embed(JSON.parse(embedText)).setTimestamp(date);
 }
