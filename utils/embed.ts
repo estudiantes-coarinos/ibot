efc81f38f02662b60fe8c85b2e1a41aaf037f9b7
Angel Johank <angeljohank@gmail.com>
2022-10-23 21:20:28 -0500

    more refactoring

--- utils/embed.ts
+++ utils/embed.ts
@@ -1,15 +1,20 @@
-import { Embed, EmbedPayload } from "https://deno.land/x/harmony@v2.6.0/mod.ts";
+import { Embed } from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 
 /**
  * Reads a json file from embeds dir and return an Embed with
  * the currect timestamp
  */
-export function getEmbed(filename: string): Embed {
+export function getEmbed(embedName: string): Embed {
   // read file contents
-  const fileDir = Deno.cwd() + "/embeds/" + filename + ".json";
-  const fileContent = Deno.readTextFileSync(fileDir);
+  const embedPath = getEmbedPath(embedName);
+  const embedText = Deno.readTextFileSync(embedPath);
 
   // create an embed
-  const payload: EmbedPayload = JSON.parse(fileContent);
-  return new Embed(payload).setTimestamp(Date.now());
+  const date = Date.now();
+  return new Embed(JSON.parse(embedText)).setTimestamp(date);
+}
+
+function getEmbedPath(embedName: string) {
+  const cwd = Deno.cwd();
+  return `${cwd}/embeds/${embedName}.json`;
 }
